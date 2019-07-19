024a8a7b681e965db4a6c48ad3136c0b264d6474
Eric Waxler <ericwaxler@gmail.com>
2019-07-19 19:24:29 -0400

    added config var

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,3 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoibm9yYWxlYmUiLCJhIjoiY2p4aGxvb2ZkMGd6ajN5cXRicDNibG82ciJ9.I70hnoEXQzfAaTYA22WJuw";
+//const API_KEY = "pk.eyJ1Ijoibm9yYWxlYmUiLCJhIjoiY2p4aGxvb2ZkMGd6ajN5cXRicDNibG82ciJ9.I70hnoEXQzfAaTYA22WJuw";
+const API_KEY = process.env.KEY;
